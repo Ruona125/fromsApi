de69ca607561e1b6e9abea4ac64f17b2a3a45016
ruona <ogheneruonaagadagba4@gmail.com>
2022-01-31 20:17:16 +0100

    about to start the API testing, already installed jest

---
+++ routes/billing.test.js
@@ -0,0 +1,10 @@
+describe("Test GET /billing", () => {
+  test("it should respond with 200", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /billing", () => {
+  test("it should respond with 200", () => {});
+});
---
+++ routes/registration.test.js
@@ -0,0 +1,10 @@
+describe("Test GET /register", () => {
+  test("it should respond with 200", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /register", () => {
+  test("it should respond with 200", () => {});
+});
